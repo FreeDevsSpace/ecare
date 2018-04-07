0f2e0ad6ba9ea60a023dc169d8969ebef11e7198
Unknown <zalakbutani87@gmail.com>
2018-04-08 00:19:33 +0530

    database sql file updated

--- ecare-API/database/ecare.sql
+++ ecare-API/database/ecare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2018 at 05:17 PM
+-- Generation Time: Apr 07, 2018 at 08:47 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.1.14
 
@@ -73,7 +73,12 @@ CREATE TABLE `doctor_registration` (
 --
 
 INSERT INTO `doctor_registration` (`Doctor_ID`, `Full_Name`, `Mobile_Number`, `DOB`, `Address`, `City`, `Email`, `Password`, `Qualification`, `Specialization`, `Hospital_Name`, `Hospital_Address`, `Fees`, `salt`) VALUES
-(6, 'Aayush Patel', '9494649', '22-3-2018', 'Address :', 'Ahmedabad', 'jjj', 'MtdjVkMaUj3y55vO9IZlazeb9rtmZjA5M2VhMTdk', 'MBBS', 'Surgeons', 'Sanjivni ', 'gota', '5946', 'ff093ea17d');
+(6, 'Aayush Patel', '9494649', '22-3-2018', 'Address :', 'Ahmedabad', 'jjj', 'MtdjVkMaUj3y55vO9IZlazeb9rtmZjA5M2VhMTdk', 'MBBS', 'Surgeons', 'Sanjivni ', 'gota', '5946', 'ff093ea17d'),
+(7, 'aarya patel', '9638527410', '7-3-1994', 'Address :', 'Ahmedabad', 'aarya', '2Kzk1r+2sjXQ1kAUwSCEdaiSxAU2NmM2MDJiNzk5', 'MBBS', 'Surgeons', 'vshaja', 'jsiska', '120', '66c602b799'),
+(8, 'Nishchal Dadhaniya', '9426672915', '12-1-1987', 'Address :', 'Gandhinagar', 'nishchal', 'HZ1LEVvzwh4PWDqWwnUoy4JaXTU0ZTU1MTQyOGFh', 'Ph.D.', 'Dermatology', 'pankti mul', 'hauaja', '250', '4e551428aa'),
+(9, 'khyat patel', '965823741', '7-1-1990', 'Address :', 'Gandhinagar', 'khyat', 'JtAxRsBPzqzVuohoHV4wdTrf0FA1YmM4YmQ1Y2Nh', 'BDS', 'Dermatology', 'haiaia', 'hsuajak', '200', '5bc8bd5cca'),
+(10, 'manushi chillar', '8258963508', '7-3-1981', 'Address :', 'Ahmedabad', 'manushi', 'IHKRmjOMBXpDfTLKtwMREbieii4xOTBkYjRkZmM0', 'BDS', 'Radiology', 'hahahab', 'hsuaja', '300', '190db4dfc4'),
+(11, 'Preety Agrawal', '8659325647', '7-3-1973', 'Address :', 'Gandhinagar', 'preety', 'zAtLHWTD2PAE1Jk7HT+YwkYtgbwxNDE4YWYwYWZk', 'BDS', 'Surgeons', 'bajama', 'uaiaka', '150', '1418af0afd');
 
 -- --------------------------------------------------------
 
@@ -100,7 +105,10 @@ CREATE TABLE `pathologist_registration` (
 
 INSERT INTO `pathologist_registration` (`Pathologist_ID`, `Full_Name`, `Mobile_Number`, `DOB`, `Qualification`, `Address`, `Blood_Group`, `Email`, `PSW`, `salt`) VALUES
 (1, 'hwhs', '646464', '94646', 'M.D.', 'vsvah\n', 'A+', 'bb', 'aQkk6Fl9ogP1EVMMvamdhm2oR/ljYjQzOTlhZTJm', 'cb4399ae2f'),
-(2, '', '', '', 'M.D.', '', 'A+', '', '5WlZE3PUV0ytgMM5jHQTChECM+g4Y2E1ZDkxZmFi', '8ca5d91fab');
+(3, 'Anurag Basu', '9568356258', '7-4-1976', 'M.Sc.', '', 'A+', 'anurag', 'V7bRERuOc7lAgrlzuX4udUhFTpgxNDllNDY0YTlk', '149e464a9d'),
+(4, 'Parag vachhani', '8685985214', '20-1-1993', 'M.D.', 'hajajak', 'A+', 'parag', 'zzRjoijVnHZ8LWeHne1IgeCtpAswNzVjY2FhNGU5', '075ccaa4e9'),
+(5, 'Pari vyas', '9425807365', '8-4-2018', 'M.Sc.', 'haiakak', 'A+', 'pari', 'WwSyvNrIMNY/ck0x+EwapWD75yYwNDMwMDYyNzlk', '043006279d'),
+(6, 'asd', '83837', '6/5/', 'M.D.', 'xgxhx', 'A+', 'asd', 'PgMF/iGyV6xG+f3TBmpOvMAYNoEzYTQ3MGRhYTYw', '3a470daa60');
 
 -- --------------------------------------------------------
 
@@ -126,7 +134,10 @@ CREATE TABLE `patient_registration` (
 
 INSERT INTO `patient_registration` (`Patient_ID`, `Full_Name`, `Mobile_Number`, `DOB`, `Email`, `Address`, `Blood_Group`, `PSW`, `salt`) VALUES
 (1, 'hsjs', '8464654236', '22-3-2005', 'a', 'gsysja', 'A+', 'CFl0RCX/bT9X+OC1vcn157vytWgwNDY3YjM4NjA0', '0467b38604'),
-(2, 'hauahs', '946464', '12-3-2018', 'abb', 'shwnan', 'A+', 'e9fRgVeTlC/4NwYbizsuA93r+Ck1YmE5MWZlODRl', '5ba91fe84e');
+(2, 'hauahs', '946464', '12-3-2018', 'abb', 'shwnan', 'A+', 'e9fRgVeTlC/4NwYbizsuA93r+Ck1YmE5MWZlODRl', '5ba91fe84e'),
+(3, 'bansi', '8585836950', '4-4-2011', 'bansi', 'ahiakama', 'O-', 'qulYCp69UwyaQzLPq/YkoJR/4qhiMzg4MjllNTA3', 'b38829e507'),
+(4, 'Chirag patel', '7878596523', '30-4-1994', 'chirag', 'bauaka', 'A+', 'CD+KsLX1n0MK/biaRv+XoyMSNcRmYmMxNjk3NjQx', 'fbc1697641'),
+(5, 'Anila patel', '9465820718', '5-4-1991', 'anila', 'ahiaka', 'A+', 'RmIJE2nzRQvIWS86CvMqXkajV6liYjdkZTk2NDk2', 'bb7de96496');
 
 -- --------------------------------------------------------
 
@@ -152,7 +163,11 @@ CREATE TABLE `pharmacist_registration` (
 --
 
 INSERT INTO `pharmacist_registration` (`Pharmacist_ID`, `Full_Name`, `Mobile_Number`, `DOB`, `Qualification`, `Address`, `Blood_Group`, `Email`, `PSW`, `salt`) VALUES
-(1, 'gg', '88', '8-4-2018', 'B.Pharm', 'gfgg', 'A+', 'll', 'T8HPuNHG2//5zTu5/1RfRe44U2I4YzRkMjZjOGE1', '8c4d26c8a5');
+(1, 'gg', '88', '8-4-2018', 'B.Pharm', 'gfgg', 'A+', 'll', 'T8HPuNHG2//5zTu5/1RfRe44U2I4YzRkMjZjOGE1', '8c4d26c8a5'),
+(2, 'cccc', '68668', '5-8-1987', 'B.Pharm', 'vjcuc', 'A+', 'cccc', 'eQ80mh4PfBRMsxRj43rfH67c9jw5MmY3ZmZmMDFl', '92f7fff01e'),
+(3, 'ggg', '832838255', '7-11-1978', 'B.Pharm', 'cycuc', 'A+', 'ggg', 'YoleRk6j98JsgOAXaqdJsjyzzE0yZTM3MzJiNmY3', '2e3732b6f7'),
+(4, 'hasvi', '943494', '17-7-1991', 'Ph.D.', 'gsuqia', 'A+', 'hasvi', 'InsOulInrYhVgUPrnQZpj85s98NjZWYzN2IxMTIz', 'cef37b1123'),
+(5, 'Kavya', '8529637410', '17-9-1992', 'B.Pharm', 'hauaikq', 'A+', 'kavya', 'z/JouoTj20TDyPPiw2UmpwCHk244OWY5ZDRjNDVh', '89f9d4c45a');
 
 --
 -- Indexes for dumped tables
@@ -205,25 +220,25 @@ ALTER TABLE `consultation`
 -- AUTO_INCREMENT for table `doctor_registration`
 --
 ALTER TABLE `doctor_registration`
-  MODIFY `Doctor_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `Doctor_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `pathologist_registration`
 --
 ALTER TABLE `pathologist_registration`
-  MODIFY `Pathologist_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Pathologist_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `patient_registration`
 --
 ALTER TABLE `patient_registration`
-  MODIFY `Patient_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Patient_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `pharmacist_registration`
 --
 ALTER TABLE `pharmacist_registration`
-  MODIFY `Pharmacist_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Pharmacist_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
